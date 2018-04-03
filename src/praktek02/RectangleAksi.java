061617a0d6b9d916d5ef681660563a1403869748
Muhammad Sauqi Abdi <muhammadsauqiabdi7@gmail.com>
2018-04-03 18:00:39 +0700

    Menigisi nilai Atribute

--- src/praktek02/RectangleAksi.java
+++ src/praktek02/RectangleAksi.java
@@ -1,6 +1,8 @@
 package praktek02;
 public class RectangleAksi {
     public static void main(String[] args) {
-     Rectangle r1 = new Rectangle();    
+     Rectangle r1 = new Rectangle();
+     r1.panjang = 10;
+     r1.lebar = 5;
     }
 }
